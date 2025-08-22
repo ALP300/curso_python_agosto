3d702c7ffe04e48371f9caed19b47c6ba7f9932e
ALP300 <aitorpalomino1@gmail.com>
2025-08-21 20:25:06 -0500

    a

---
+++ EJERCICIOS/lista.py
@@ -0,0 +1,9 @@
+#LISTAS
+frutas=["manzana","banana","naranja",{"nombre":"Leo"}]
+frutas.append("pera")
+frutas[1]= "mango"
+frutas.remove("naranja")
+print(f"La fruta en la posición 0 es: {frutas[-1]}")
+print("La lista total es: ", frutas)
+
+
